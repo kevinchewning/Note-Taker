14e49ab70c14269163f8e0eb3519c4d6887e4354
kevinchewning <klcbusiness@hotmail.com>
2021-08-15 12:43:17 -0400

    corrected port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const util = require('util');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
